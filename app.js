eb247ac8aacdd56e61eb3bcddc28b2e258ed3319
Thomas Feijoo <thomwe8@hotmail.com>
2021-03-28 19:41:56 -0300

    Conditional Rendering

--- app.js
+++ app.js
@@ -2,14 +2,15 @@ const app = Vue.createApp({
     // data or functions to react to events
     data() {
         return {
+            showBooks: true,
             title: 'The Final Empire',
             author: 'Brandon Sanderson',
             age: 45
         }
     },
     methods: {
-        changeTitle(title) {
-            this.title = title;
+        toggleShowBooks() {
+            this.showBooks = !this.showBooks
         }
     }
 });
