6351a8d9ed0626695f129e373b982dd22dacadf2
Thomas Feijoo <thomwe8@hotmail.com>
2021-03-28 20:04:47 -0300

    v for

--- app.js
+++ app.js
@@ -3,26 +3,16 @@ const app = Vue.createApp({
     data() {
         return {
             showBooks: true,
-            title: 'The Final Empire',
-            author: 'Brandon Sanderson',
-            age: 45,
-            x: 0,
-            y: 0
+            books: [
+                { title: 'name of the wind', author: 'patrick rothfuss' },
+                { title: 'lord of the rings', author: 'tolkien' },
+                { title: 'witcher', author: 'andrzej sapkowski' }
+            ]
         }
     },
     methods: {
         toggleShowBooks() {
             this.showBooks = !this.showBooks
-        },
-        handleEvent(event, data) {
-            console.log(event, event.type);
-            if (data) {
-                console.log(data);
-            }
-        },
-        handleMouseMove(event) {
-            this.x = event.offsetX;
-            this.y = event.offsetY;
         }
     }
 });
