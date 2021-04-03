022f3b5f9fc94dcecdf1bdac9ae8225889f1f50e
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-02 22:19:40 -0300

    emitting a close event from modal to app

--- Section-3/modal-project/src/App.vue
+++ Section-3/modal-project/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal :header="header" :text="text" theme="sale"/>
+  <p>Welcome...</p>
+  <div v-if="showModal">
+    <Modal :header="header" :text="text" theme="sale" @close="toggleModal"/>
+  </div>
+  <button @click="toggleModal">open modal</button>
 </template>
 
 <script>
@@ -15,9 +19,15 @@ export default {
     return {
       title: 'My First Vue maoe App',
       header: 'Sign up for the giveaway',
-      text: 'Win rewards'
+      text: 'Win rewards',
+      showModal: false
     }
   },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal
+    }
+  }
 }
 </script>
 
