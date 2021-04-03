9eb44c6dcacb3c7d6acb57fd289a3ed6e8965379
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-03 17:51:38 -0300

    deleting hello world and editing some html and routes

--- Section-6-Routing/ninja-jobs/src/views/Home.vue
+++ Section-6-Routing/ninja-jobs/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Homepage</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, repellat, optio aperiam repellendus animi dolorem, similique voluptates cupiditate ipsam dicta soluta? Eius fuga, esse tempora magnam tenetur sequi possimus maxime harum quisquam laudantium nihil ipsam quod facere eaque iusto quasi in. Eligendi in magni perferendis rerum aspernatur sapiente esse doloribus, architecto et qui odio provident laboriosam, amet nostrum dolore sequi mollitia earum cum fuga eius nobis animi, molestiae facilis autem. Optio animi nemo enim iusto molestias voluptate perferendis, ipsa impedit eum amet obcaecati maxime culpa est unde quia, dolore recusandae! Pariatur quos magni blanditiis debitis. Deleniti architecto maiores similique dolore?</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sint, repellat, optio aperiam repellendus animi dolorem, similique voluptates cupiditate ipsam dicta soluta? Eius fuga, esse tempora magnam tenetur sequi possimus maxime harum quisquam laudantium nihil ipsam quod facere eaque iusto quasi in. Eligendi in magni perferendis rerum aspernatur sapiente esse doloribus, architecto et qui odio provident laboriosam, amet nostrum dolore sequi mollitia earum cum fuga eius nobis animi, molestiae facilis autem. Optio animi nemo enim iusto molestias voluptate perferendis, ipsa impedit eum amet obcaecati maxime culpa est unde quia, dolore recusandae! Pariatur quos magni blanditiis debitis. Deleniti architecto maiores similique dolore?</p>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  components: {}
 }
 </script>
