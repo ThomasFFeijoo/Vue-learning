5e4fd791ca581979b6da3a5a7a29d706a19973e7
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-02 22:11:22 -0300

    added some props to the modal

--- Section-3/modal-project/src/App.vue
+++ Section-3/modal-project/src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
+  <Modal :header="header" :text="text" theme="sale"/>
 </template>
 
 <script>
@@ -13,7 +13,9 @@ export default {
   },
   data() {
     return {
-      title: 'My First Vue maoe App'
+      title: 'My First Vue maoe App',
+      header: 'Sign up for the giveaway',
+      text: 'Win rewards'
     }
   },
 }
--- Section-3/modal-project/src/components/Modal.vue
+++ Section-3/modal-project/src/components/Modal.vue
@@ -1,10 +1,17 @@
 <template>
     <div class="backdrop">
-        <div class="modal">
-            <p>modal content thomasssss</p>
+        <div class="modal" :class="{ sale: theme === 'sale' }">
+            <h1>{{ header }}</h1>
+            <p>{{ text }}</p>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    props: ['header', 'text', 'theme']
+}
+</script>
 
 <style>
     .modal {
@@ -25,5 +32,15 @@
         color: #03cfb4;
         border: none;
         padding: 0;
+    }
+    .modal p {
+        font-style: normal;
+    }
+    .modal.sale {
+        background: crimson;
+        color: white
+    }
+    .modal.sale h1 {
+        color: white;
     }
 </style>
