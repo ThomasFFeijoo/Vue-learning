8c40c47e5a92ac93e2423a6909d11ba3a9a08dc3
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-03 16:51:35 -0300

    inputting some text with keyup

--- Section-5-Web-Form/web-form/src/components/SignupForm.vue
+++ Section-5-Web-Form/web-form/src/components/SignupForm.vue
@@ -12,29 +12,22 @@
         <option value="designer">Web Designer</option>
       </select>
 
+      <label>Skills:</label>
+      <input type="text" v-model="tempSkill" @keyup.alt="addSkill">
+      <div v-for="skill in skills" :key="skill" class="pill">
+        {{ skill }}
+      </div>
+
       <div class="terms">
         <input type="checkbox" v-model="terms" required>
         <label>Accept terms and conditions</label>
       </div>
 
-      <div>
-        <input type="checkbox" value="thomas" v-model="names">
-        <label>Thomas</label>
-      </div>
-      <div>
-        <input type="checkbox" value="nicole" v-model="names">
-        <label>Nicole</label>
-      </div>
-      <div>
-        <input type="checkbox" value="thati" v-model="names">
-        <label>Thati</label>
-      </div>
   </form>
   <p>Email: {{ email }}</p>
   <p>Password: {{ password }}</p>
   <p>Role: {{ role }}</p>
   <p>Terms: {{ terms }}</p>
-  <p>Names: {{ names }}</p>
 </template>
 
 <script>
@@ -45,7 +38,18 @@ export default {
       password: '',
       role: 'designer',
       terms: false,
-      names: []
+      tempSkill: '',
+      skills: []
+    }
+  },
+  methods: {
+    addSkill(e) {
+      if(e.key === ',' && this.tempSkill) {
+        if(!this.skills.includes(this.tempSkill)) {
+          this.skills.push(this.tempSkill)
+        }
+        this.tempSkill = ''
+      }
     }
   }
 }
