7c74535fe395a28a21948fe7e3c6a0cca4f48d64
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-07 23:45:55 -0300

    added components to highlight props use

--- Section-9-Dojo-Blog/dojo-blog/src/views/Home.vue
+++ Section-9-Dojo-Blog/dojo-blog/src/views/Home.vue
@@ -1,42 +1,24 @@
 <template>
   <div class="home">
     <h1>Home</h1>
-    <input type="text" v-model="search">
-    <p>search term - {{ search }}</p>
-    <div v-for="name in matchingNames" :key="name">
-      {{ name }}
-    </div>
-    <button @click="handleClick">stop watching</button>
+    <PostList :posts="posts" />
   </div>
 </template>
 
 <script>
-import { computed, ref, watch, watchEffect } from 'vue'
+import PostList from '../components/PostList.vue'
+import { ref } from 'vue'
 
 export default {
   name: 'Home',
+  components: { PostList },
   setup() {
-    const search = ref('')
-    const names = ref(['mario', 'luigi', 'yoshi', 'toad', 'bowser', 'koopa', 'peach'])
-
-    const stopWatch = watch(search, () => {
-      console.log('watch function ran')
-    })
-
-    const stopEffect = watchEffect(() => {
-      console.log('watch effect function ran', search.value)
-    })
-
-    const matchingNames = computed(() => {
-      return names.value.filter((name) => name.includes(search.value))
-    })
-
-    // calling the functions make the watch stop
-    const handleClick = () => {
-      stopWatch()
-      stopEffect()
-    }
-    return { names, search, matchingNames, handleClick }
+    const posts = ref([
+      { title: 'welcome to the blog', body: 'lorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsumlorem ipsum', id: 1 },
+      { title: 'top 5 css tips', body: 'lorem ipsum', id: 1 }
+    ])
+    
+    return { posts }
   }
 }
 </script>
