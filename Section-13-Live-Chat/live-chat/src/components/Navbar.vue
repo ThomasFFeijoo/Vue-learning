5efec7041b9b8868291508365bff93f302483b1b
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-14 20:50:04 -0300

    showing logged user info

--- Section-13-Live-Chat/live-chat/src/components/Navbar.vue
+++ Section-13-Live-Chat/live-chat/src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
+  <nav v-if="user">
       <div>
-          <p>hey there... display name here</p>
-          <p class="email">currently logged in as... email</p>
+          <p>Hey there {{ user.displayName }}</p>
+          <p class="email">currently logged in as {{ user.email }}</p>
       </div>
       <button @click="handleClick">Logout</button>
   </nav>
@@ -10,11 +10,12 @@
 
 <script>
 import useLogout from '../composables/useLogout'
+import getUser from '../composables/getUser'
 
 export default {
     setup() {
         const { logout, error } = useLogout()
-
+        const { user } = getUser()
         const handleClick = async () => {
             await logout()
             if(!error.value) {
@@ -22,7 +23,7 @@ export default {
             }
         }
 
-        return { handleClick }
+        return { handleClick, user }
     }
 }
 </script>
