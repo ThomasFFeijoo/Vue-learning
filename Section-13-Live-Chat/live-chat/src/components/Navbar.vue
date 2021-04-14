30aad0be7c89a3d51eea76c3ad7f7447af27de84
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-14 20:33:59 -0300

    added navbar to chatroom

---
+++ Section-13-Live-Chat/live-chat/src/components/Navbar.vue
@@ -0,0 +1,19 @@
+<template>
+  <nav>
+      <div>
+          <p>hey there... display name here</p>
+          <p class="email">currently logged in as... email</p>
+      </div>
+      <button>Logout</button>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
