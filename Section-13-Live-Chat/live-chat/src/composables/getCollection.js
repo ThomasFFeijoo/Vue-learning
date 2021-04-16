05d9313c47fd8477cdc5cc2c89d37cd7ba6e37e6
Thomas F Feijoo <thomwe8@hotmail.com>
2021-04-15 21:26:08 -0300

    using watchEffect to unsub a real time listener

--- Section-13-Live-Chat/live-chat/src/composables/getCollection.js
+++ Section-13-Live-Chat/live-chat/src/composables/getCollection.js
@@ -1,4 +1,4 @@
-import { ref } from 'vue'
+import { ref, watchEffect } from 'vue'
 import { projectFirestore } from '../firebase/config'
 
 const getCollection = (collection) => {
@@ -8,7 +8,7 @@ const getCollection = (collection) => {
     let collectionRef = projectFirestore.collection(collection)
         .orderBy('createAt')
 
-    collectionRef.onSnapshot((snap) => {
+    const unsub = collectionRef.onSnapshot((snap) => {
         let results = []
         snap.docs.forEach(doc => {
             doc.data().createAt && results.push({ ...doc.data(), id: doc.id }) //createAt && added to not use local version of snap, so this is waiting the timestamp from the server
@@ -19,6 +19,11 @@ const getCollection = (collection) => {
         console.log(err.message)
         documents.value = null
         error.value = 'could not fetch data'
+    })
+
+    watchEffect((onInvalidate) => {
+        // unsub from prev collection when watcher is stopped (component unmounted)
+        onInvalidate(() => unsub())
     })
 
     return { documents, error }
